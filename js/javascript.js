961443a4213adfe08558bcce128450f85da909cd
cesardeveloper <cesarsoares1997@gmail.com>
2021-06-10 23:21:48 -0300

    alterei layout depois do link do CSS

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,27 @@
 
+busca = document.querySelector('#busca');
 
-document.addEventListener('DOMContentLoaded', function(){
-    document.querySelector('#form').onclick = function(){
-        document.querySelector('#search');
-        alert(`Ainda não há nada para pesquisar!`)
-    };
+btn = document.querySelector('#btn');
 
-});
+btn.onclick = function(){
+
+    removerSpans();
+
+    let span = document.createElement('span');
+    span.setAttribute('class', 'alert-warning');
+
+    let msg = document.createTextNode('Não há nada ainda para procurar');
+    
+    span.appendChild(msg);
+
+    busca.appendChild(span);
+}
+
+function removerSpans() {
+    let spans = document.querySelectorAll('span');
+        
+    for(let i = 0; i < spans.length; i++){
+        busca.removeChild(spans[i]);
+    }
+
+}
